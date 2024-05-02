da6438534bd74bfb3e9fcfb55a09e63a1d662cb6
XnView <contact@xnview.com>
2024-05-02 09:14:50 +0000

    Translated using Weblate (Slovak)
    Currently translated at 97.5% (4212 of 4318 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/sk/

--- XnViewMP/xnview_sk.ts
+++ XnViewMP/xnview_sk.ts
@@ -17462,7 +17462,7 @@ Do you want to delete the favorite &quot;%1&quot;?</source>
         <location filename="TemplateData.cpp" line="2534"/>
         <source>Shutter speed (s)</source>
         <oldsource>Shutter speed [s]</oldsource>
-        <translation type="unfinished">Rýchlosť uzávierky [s]</translation>
+        <translation>Rýchlosť uzávierky (s)</translation>
     </message>
     <message>
         <location filename="TemplateData.cpp" line="2536"/>
