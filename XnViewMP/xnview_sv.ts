c1082ae9438f843acc33b9b21d595dfedfab4f25
XnView <contact@xnview.com>
2024-05-02 09:15:02 +0000

    Translated using Weblate (Swedish)
    Currently translated at 97.7% (4223 of 4318 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/sv/

--- XnViewMP/xnview_sv.ts
+++ XnViewMP/xnview_sv.ts
@@ -17458,7 +17458,7 @@ Do you want to delete the favorite &quot;%1&quot;?</source>
         <location filename="TemplateData.cpp" line="2534"/>
         <source>Shutter speed (s)</source>
         <oldsource>Shutter speed [s]</oldsource>
-        <translation type="unfinished">Slutarhastighet [s]</translation>
+        <translation>Slutarhastighet (s)</translation>
     </message>
     <message>
         <location filename="TemplateData.cpp" line="2536"/>
