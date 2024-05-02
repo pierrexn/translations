8a36971101d8c2a355579e8f293da98841fcc3c8
XnView <contact@xnview.com>
2024-05-02 09:14:04 +0000

    Translated using Weblate (Estonian)
    Currently translated at 51.6% (2229 of 4318 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/et/

--- XnViewMP/xnview_et.ts
+++ XnViewMP/xnview_et.ts
@@ -17438,7 +17438,7 @@ Do you want to delete the favorite &quot;%1&quot;?</source>
         <location filename="TemplateData.cpp" line="2534"/>
         <source>Shutter speed (s)</source>
         <oldsource>Shutter speed [s]</oldsource>
-        <translation type="unfinished">Katiku kiirus [s]</translation>
+        <translation>Katiku kiirus (s)</translation>
     </message>
     <message>
         <location filename="TemplateData.cpp" line="2536"/>
