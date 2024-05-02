b0bb18fee596367139a981403080e7fb8ea4f5cd
XnView <contact@xnview.com>
2024-05-02 09:14:27 +0000

    Translated using Weblate (Korean)
    Currently translated at 97.7% (4223 of 4318 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/ko/

--- XnViewMP/xnview_ko.ts
+++ XnViewMP/xnview_ko.ts
@@ -17393,7 +17393,7 @@ Do you want to delete the favorite &quot;%1&quot;?</source>
         <location filename="TemplateData.cpp" line="2534"/>
         <source>Shutter speed (s)</source>
         <oldsource>Shutter speed [s]</oldsource>
-        <translation type="unfinished">셔터 속도 [s]</translation>
+        <translation>셔터 속도 (s)</translation>
     </message>
     <message>
         <location filename="TemplateData.cpp" line="2536"/>
