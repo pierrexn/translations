3ac2adcba0e6a3d29702402af7c9a6a5d215f2b6
user0 <gr2y.prv@gmail.com>
2025-03-22 14:16:12 +0000

    Translated using Weblate (Armenian)
    Currently translated at 97.0% (4151 of 4275 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/hy/

--- XnViewMP/xnview_hy.ts
+++ XnViewMP/xnview_hy.ts
@@ -12138,7 +12138,7 @@ Do you want to continue?</source>
     </message>
     <message>
         <source>4:2:2 (2x1,1x1,1x1)</source>
-        <translation type="unfinished">2х1, 1x1, 1x1 (4:2:2) {4:2:2 (2x?} {1,1x?} {1,1x?} {1)?}</translation>
+        <translation>4:2:2 (2x1,1x1,1x1)</translation>
     </message>
     <message>
         <source>4:4:4 (1x1,1x1,1x1) best quality</source>
