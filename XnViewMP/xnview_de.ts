1493ca2bebc7ae42ceafada933800373d9d0544a
user0 <gr2y.prv@gmail.com>
2025-03-22 14:11:49 +0000

    Translated using Weblate (German)
    Currently translated at 99.6% (4260 of 4275 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/de/

--- XnViewMP/xnview_de.ts
+++ XnViewMP/xnview_de.ts
@@ -15671,13 +15671,13 @@ Wollen Sie fortfahren?</translation>
         <location filename="DlgWriteJPEG.ui" line="206"/>
         <source>4:2:2 (2x1,1x1,1x1)</source>
         <oldsource>2x1,1x1,1x1 (4:2:2)</oldsource>
-        <translation type="unfinished">2x1,1x1,1x1 (4:2:2)</translation>
+        <translation>4:2:2 (2x1,1x1,1x1)</translation>
     </message>
     <message>
         <location filename="DlgWriteJPEG.ui" line="211"/>
         <source>4:4:4 (1x1,1x1,1x1) best quality</source>
         <oldsource>1x1,1x1,1x1 (best quality)</oldsource>
-        <translation type="unfinished">1x1,1x1,1x1 (Beste Qualität)</translation>
+        <translation>4:4:4 (1x1,1x1,1x1) Beste Qualität</translation>
     </message>
     <message>
         <location filename="DlgWriteJPEG.ui" line="236"/>
