4f5014d61c9a40f2d747fe4ba07dcdea7409c846
user0 <gr2y.prv@gmail.com>
2025-03-23 15:36:25 +0000

    Translated using Weblate (Chinese (Simplified Han script))
    Currently translated at 89.0% (3806 of 4273 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/zh_Hans/

--- XnViewMP/xnview_zh_CN.ts
+++ XnViewMP/xnview_zh_CN.ts
@@ -7741,7 +7741,7 @@ Please make sure that files are not read-only!</oldsource>
         <location filename="DlgExport.ui" line="451"/>
         <source>4:4:4 (1x1,1x1,1x1) best quality</source>
         <oldsource>1x1,1x1,1x1 (best quality)</oldsource>
-        <translation type="unfinished">1x1,1x1,1x1 (最高品质)</translation>
+        <translation>4:4:4 (1x1,1x1,1x1) 最高品质</translation>
     </message>
     <message>
         <location filename="DlgExport.ui" line="610"/>
