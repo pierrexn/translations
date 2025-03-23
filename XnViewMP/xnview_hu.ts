4a997846928979a4edfe3db84c896795948a06c0
user0 <gr2y.prv@gmail.com>
2025-03-23 15:37:21 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (4273 of 4273 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/hu/

--- XnViewMP/xnview_hu.ts
+++ XnViewMP/xnview_hu.ts
@@ -7747,7 +7747,7 @@ Ellenőrizze, hogy a fájl nem írásvédett!</translation>
         <location filename="DlgExport.ui" line="451"/>
         <source>4:4:4 (1x1,1x1,1x1) best quality</source>
         <oldsource>1x1,1x1,1x1 (best quality)</oldsource>
-        <translation type="unfinished">1x1,1x1,1x1 (legjobb minőség)</translation>
+        <translation>4:4:4 (1x1,1x1,1x1) legjobb minőség</translation>
     </message>
     <message>
         <location filename="DlgExport.ui" line="610"/>
