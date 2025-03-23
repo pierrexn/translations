e4a6e43250b0434983fe77ccee7d701faade0202
user0 <gr2y.prv@gmail.com>
2025-03-23 15:38:16 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 76.8% (3284 of 4273 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/nb_NO/

--- XnViewMP/xnview_no.ts
+++ XnViewMP/xnview_no.ts
@@ -7735,7 +7735,7 @@ Please make sure that files are not read-only!</oldsource>
         <location filename="DlgExport.ui" line="451"/>
         <source>4:4:4 (1x1,1x1,1x1) best quality</source>
         <oldsource>1x1,1x1,1x1 (best quality)</oldsource>
-        <translation type="unfinished">1x1,1x1,1x1 (beste kvalitet)</translation>
+        <translation>4:4:4 (1x1,1x1,1x1) beste kvalitet</translation>
     </message>
     <message>
         <location filename="DlgExport.ui" line="610"/>
