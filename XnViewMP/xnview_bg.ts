5efa2ae68b9598558d17dd3e1472d3dbfac9fcc8
user0 <gr2y.prv@gmail.com>
2025-03-23 15:36:04 +0000

    Translated using Weblate (Bulgarian)
    Currently translated at 91.5% (3911 of 4273 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/bg/

--- XnViewMP/xnview_bg.ts
+++ XnViewMP/xnview_bg.ts
@@ -7735,7 +7735,7 @@ Please make sure that files are not read-only!</oldsource>
         <location filename="DlgExport.ui" line="451"/>
         <source>4:4:4 (1x1,1x1,1x1) best quality</source>
         <oldsource>1x1,1x1,1x1 (best quality)</oldsource>
-        <translation type="unfinished">1x1,1x1,1x1 (най-добро качество)</translation>
+        <translation>4:4:4 (1x1,1x1,1x1) най-добро качество</translation>
     </message>
     <message>
         <location filename="DlgExport.ui" line="610"/>
