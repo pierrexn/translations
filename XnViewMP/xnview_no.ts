4b68aa5668e53a747c840322027a5dc03de5fff4
user0 <gr2y.prv@gmail.com>
2025-09-16 12:58:09 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 74.7% (3228 of 4317 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/nb_NO/

--- XnViewMP/xnview_no.ts
+++ XnViewMP/xnview_no.ts
@@ -17468,7 +17468,7 @@ Do you want to delete the favorite &quot;%1&quot;?</source>
     <message>
         <location filename="DBFilterModel.cpp" line="1470"/>
         <source>Shutter Speed</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Lukker hastighet</translation>
     </message>
     <message>
         <location filename="ItemModel.cpp" line="1127"/>
