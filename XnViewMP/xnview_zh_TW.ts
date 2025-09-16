20546c03d307e6520851c9990ec4cde1ac4825da
user0 <gr2y.prv@gmail.com>
2025-09-16 12:53:55 +0000

    Translated using Weblate (Chinese (Traditional Han script))
    Currently translated at 89.5% (3865 of 4317 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/zh_Hant/

--- XnViewMP/xnview_zh_TW.ts
+++ XnViewMP/xnview_zh_TW.ts
@@ -17475,7 +17475,7 @@ Do you want to delete the favorite &quot;%1&quot;?</source>
     <message>
         <location filename="DBFilterModel.cpp" line="1470"/>
         <source>Shutter Speed</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">快門速度</translation>
     </message>
     <message>
         <location filename="ItemModel.cpp" line="1127"/>
