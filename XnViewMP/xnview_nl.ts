15485161551da830ebfbe09bdd216900b1607689
Stephan Paternotte <stephan@paternottes.net>
2025-09-26 02:51:53 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (4336 of 4336 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/nl/

--- XnViewMP/xnview_nl.ts
+++ XnViewMP/xnview_nl.ts
@@ -11695,7 +11695,7 @@ Wilt u items verwijderen die zijn toegewezen aan een categorie?</translation>
         <location filename="DlgSettingsCache.ui" line="430"/>
         <source>Update metadata...</source>
         <oldsource>Update metadatas...</oldsource>
-        <translation type="unfinished">Metagegevens bijwerken…</translation>
+        <translation>Metagegevens bijwerken…</translation>
     </message>
     <message>
         <location filename="DlgSettingsCache.ui" line="493"/>
@@ -18040,17 +18040,17 @@ Wil je de favoriet &quot;%1&quot; verwijderen&quot;?</translation>
     <message>
         <location filename="InfoFavoritesView.cpp" line="19"/>
         <source>Tag</source>
-        <translation type="unfinished">Label</translation>
+        <translation>Label</translation>
     </message>
     <message>
         <location filename="InfoFavoritesView.cpp" line="19"/>
         <source>Value</source>
-        <translation type="unfinished">Waarde</translation>
+        <translation>Waarde</translation>
     </message>
     <message>
         <location filename="InfoFavoritesView.cpp" line="45"/>
         <source>Remove from Favorites panel</source>
-        <translation type="unfinished"></translation>
+        <translation>Verwijderen uit het deelvenster Favorieten</translation>
     </message>
 </context>
 <context>
@@ -18812,7 +18812,7 @@ Wil je de favoriet &quot;%1&quot; verwijderen&quot;?</translation>
     <message>
         <location filename="InfoPanel.cpp" line="91"/>
         <source>Add to Favorites panel</source>
-        <translation type="unfinished"></translation>
+        <translation>Toevoegen aan het deelvenster Favorieten</translation>
     </message>
 </context>
 <context>
