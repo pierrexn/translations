de8d08986b56a29dc9cb09712205ed3d4c2f6824
Stephan Paternotte <stephan@paternottes.net>
2025-10-17 04:40:50 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (4339 of 4339 strings)
    
    Translation: XnView MP/General
    Translate-URL: https://translation.xnview.com/projects/xnview-mp/general/nl/

--- XnViewMP/xnview_nl.ts
+++ XnViewMP/xnview_nl.ts
@@ -185,12 +185,12 @@
         <location filename="AboutWidget.ui" line="275"/>
         <location filename="AboutWidget.ui" line="299"/>
         <source>Translators</source>
-        <translation type="unfinished"></translation>
+        <translation>Vertalers</translation>
     </message>
     <message>
         <location filename="AboutWidget.ui" line="294"/>
         <source>Language</source>
-        <translation type="unfinished">Taal</translation>
+        <translation>Taal</translation>
     </message>
     <message>
         <location filename="AboutWidget.ui" line="308"/>
